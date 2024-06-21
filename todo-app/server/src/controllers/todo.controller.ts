a9acb968560f3accf9b97b0cec5806fe1f2484b5
NikhilOli <nikhiloli107@gmail.com>
2024-06-21 21:16:27 +0545

    create and get todo

--- todo-app/server/src/controllers/todo.controller.ts
+++ todo-app/server/src/controllers/todo.controller.ts
@@ -2,12 +2,41 @@ import { RequestHandler } from "express"
 import Todo from "../models/todo.model"
 
 
-const getTodos: RequestHandler = async (req, res) =>  {    
-    const todos = await Todo.find()
+const getTodos: RequestHandler = async (req, res) =>  {  
+    try {
+        const todos = await Todo.find()
+    
+        res.status(200).send(todos[0].todo)
+        
+    } catch (error) {
+        console.error("Error getting todos",error);
+        
+    }
+}
 
-    res.status(200).send(todos[0].todo)
-    // res.send(`Hello from server running on PORT ${process.env.PORT}`);
+const createTodo: RequestHandler = async (req, res) => {
+    const todo = req.body;
+    try {
+        
+        const newTodo = Todo.create(todo)
+        res.status(201).json(newTodo)
+
+    } catch (error ) {
+        throw new Error(`Error creating todo `);
+    }
+}
+
+const getTodo: RequestHandler = async (req, res) =>  {    
+    const todoId = req.params.todoId;
+    try {
+        const todo = await Todo.findById(todoId)
+        res.status(200).json(todo)
+        
+    } catch (error) {
+        console.error("Error getting todos",error);
+        
+    }
 }
 
 
-export { getTodos }
+export { getTodos, createTodo, getTodo }
--- todo-app/server/src/routes/todos.routes.ts
+++ todo-app/server/src/routes/todos.routes.ts
@@ -5,4 +5,6 @@ import * as TodoController from "../controllers/todo.controller"
 export const todosRouter = Router();
 
 
+todosRouter.post("/api/todos", TodoController.createTodo)
 todosRouter.get("/api/todos", TodoController.getTodos)
+todosRouter.get("/api/todos/:todoId", TodoController.getTodo)
